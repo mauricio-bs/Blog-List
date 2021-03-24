63761c423d51ba9ad2ad503a4970ed5f05c65b3d
mauricio-bs <mauricio.3g2014@gmail.com>
2021-03-23 22:22:32 -0300

    configurada criação de categorias e mensagens de sucesso e erro; criado partialView para mensagens

--- app.js
+++ app.js
@@ -5,16 +5,37 @@ const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 //configurações
+    //Sessão
+        app.use(session({
+            secret: "cursonode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+
+    //Midleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("Success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
+
     //body parser
         app.use(urlencoded({extended:true}))
         app.use(json())
+    
     //handlebars
         app.engine('handlebars', handlebars({defaultLayout: 'main'}))
         app.set('view engine', 'handlebars')
+    
     //mongoose
-        mongoose.connect()
+        const connectDB = require('./database/connection')
+        connectDB()
+    
     //Public
         app.use(express.static(path.join(__dirname, 'public')))
 
@@ -24,6 +45,4 @@ const mongoose = require('mongoose')
 
 //outros
 const PORT = 8081
-app.listen(PORT, () => {
-    console.log('servidor rodando!')
-})
+app.listen(PORT, () => {console.log(`servidor rodando em http://localhost:${PORT}`)})
--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose')
 
 const uri = "mongodb+srv://admin:admin123@nodejs1.jkncm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new mongoose.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+const connectDB = async () => {
+    try{
+        //mongodb connection string
+        const con = await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
+        console.log(`MongoDB Connected: ${con.connection.host}`);
+    }
+    catch(err){
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB
